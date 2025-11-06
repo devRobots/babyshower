82e0468a40f1fd5d5ee9d5bbe32efc65ac0e64dc
DevRobot <ysrosast@gmail.com>
2025-11-06 23:59:50 +0100

    monorepo config

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+import GiftList from "@/frontend/components/GiftList";
+import Hero from "@/frontend/components/Hero";
+import Info from "@/frontend/components/Info";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Info />
+      <GiftList />
+    </>
+  );
+}
